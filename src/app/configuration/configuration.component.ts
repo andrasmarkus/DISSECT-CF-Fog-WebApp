1c500bb814ba3b809868c3fb580ff0b9477fa03e
Rudolf Bun <rudolf.bun@casrd.net>
2020-03-17 17:41:04 +0100

    Remove unnecessary comment and injection in configuration component

--- src/app/configuration/configuration.component.ts
+++ src/app/configuration/configuration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
-import { CloudNumberFormComponent } from '../cloud-number-form/cloud-number-form.component';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CloudNumberFormComponent } from './cloud-number-form/cloud-number-form.component';
 
 @Component({
   selector: 'app-configuration',
@@ -15,7 +15,7 @@ export class ConfigurationComponent implements OnInit, AfterViewChecked {
 
   @ViewChild(CloudNumberFormComponent) numOfCloudsForm: CloudNumberFormComponent;
 
-  constructor(private _formBuilder: FormBuilder, private changeDetect: ChangeDetectorRef) {}
+  constructor(private changeDetect: ChangeDetectorRef) {}
 
   ngOnInit(): void {}
 
@@ -23,13 +23,6 @@ export class ConfigurationComponent implements OnInit, AfterViewChecked {
     this.changeDetect.detectChanges();
   }
 
-  /* numOfCloudsEventHandler($event:any){
-    this.numOfClouds = $event;
-  }
-  numOfFogsEventHandler($event:any){
-    this.numOfFogs = $event;
-  }
- */
   get numOfCloudsFromGroup() {
     return this.numOfCloudsForm ? this.numOfCloudsForm.numOfComputingNodes : null;
   }
