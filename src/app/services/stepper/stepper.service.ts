e0608a12d9134dc3ad01b142268eca789b7fddcd
Rudolf <Bun.Rudolf@stud.u-szeged.hu>
2020-07-10 23:18:09 +0200

    Create stepper service for stepper navigation

---
+++ src/app/services/stepper/stepper.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { MatStepper } from '@angular/material/stepper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StepperService {
+  private matStepper: MatStepper;
+
+  constructor() {}
+
+  public setStepper(stepper: MatStepper): void {
+    this.matStepper = stepper;
+  }
+
+  public get stepper(): MatStepper {
+    return this.matStepper;
+  }
+
+  public stepBack(): void {
+    if (this.matStepper) {
+      this.matStepper.previous();
+    }
+  }
+
+  public stepForward(): void {
+    if (this.matStepper) {
+      this.matStepper.next();
+    }
+  }
+}
