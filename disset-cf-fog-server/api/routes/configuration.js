e341b9bad618db1e4e01a582d825d31bc2dc6cb3
Rudolf <Bun.Rudolf@stud.u-szeged.hu>
2020-09-27 17:24:54 +0200

    Send back the created file in response

--- disset-cf-fog-server/api/routes/configuration.js
+++ disset-cf-fog-server/api/routes/configuration.js
@@ -3,6 +3,8 @@ const router = express.Router({caseSensitive:true});
 const fs = require('fs');
 const { isEmpty } = require('lodash');
 const Parser = require("fast-xml-parser").j2xParser;
+const exec = require('child_process').exec;
+const path = require('path'); 
 
 router.get('/', (req, res , next)=> {
     res.status(200).json({
@@ -34,7 +36,42 @@ router.post('/', (req, res , next)=> {
       if (err) return console.log(err);
       console.log('devices > devices.xml');
     });
-    res.status(201).json({message: 'Created all files!'});
+
+    const appFullPath = 'target/'
+    const mainClassPath ='hu.u_szeged.inf.fog.simulator.demo.'
+    const pwd = '../'
+    const fileToSave = '../configurations/';
+
+    const cmd = 'cd dissect-cf && java -cp ' + appFullPath + 'dissect-cf-0.9.7-SNAPSHOT-jar-with-dependencies.jar ' 
+      + mainClassPath + 'CLFogSimulation ' + pwd + 'configurations/appliances.xml '
+      + pwd +'configurations/devices.xml '+ fileToSave;
+
+      exec(cmd, {maxBuffer: 1024 * 1024}, (errExec, stdout, stderr) => {
+      if (errExec) {
+        console.error(errExec);
+        return;
+      }
+      console.log(stdout);
+      const fileName = getLastCreatedHtmlFile('configurations').file;
+      const html = fs.readFileSync( 'configurations/' + fileName );
+      res.json({html: html.toString(), data: stdout});
+      /* res.sendFile(fileName, {root: 'configurations' }, (errSending) => {
+        if (errSending) {
+          res.status(500).json({message:'Can not find the generated html!'})
+        } else {
+          console.log('Sent:', fileName)
+          res.end();
+        }
+      }); */
+    });
+    const getLastCreatedHtmlFile = (dirName) => {
+      console.log(fs.readdirSync(dirName));
+      const files = fs.readdirSync(dirName)
+        .filter(file => path.extname(file) == ".html")
+        .map(file => ({ file, mtime: fs.statSync('./' + dirName + '/'+ file).mtime }))
+        .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+      return  files.length ? files[0] : undefined;
+    }
   }else{
     res.status(500).json({message:'Bad request!'})
   }
