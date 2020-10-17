2b4bcabf0bc77df3b4844db60cef17f3171c884f
Rudolf <Bun.Rudolf@stud.u-szeged.hu>
2020-10-17 23:53:34 +0200

    Implement user logout

--- disset-cf-fog-client/src/app/navigation/sidenav-items/sidenav-items.component.ts
+++ disset-cf-fog-client/src/app/navigation/sidenav-items/sidenav-items.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TokenStorageService } from 'src/app/services/token-storage/token-storage.service';
 
 @Component({
   selector: 'app-sidenav-items',
   templateUrl: './sidenav-items.component.html',
   styleUrls: ['./sidenav-items.component.css']
 })
-export class SidenavItemsComponent {}
+export class SidenavItemsComponent {
+  constructor(public tokenService: TokenStorageService) {}
+}
