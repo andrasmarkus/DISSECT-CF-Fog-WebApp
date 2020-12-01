1099bbbc251b98c6074226d9cf7c92646ae5069a
Your Name <you@example.com>
2020-12-01 16:57:51 +0100

    added HTML tags

--- dissect-cf-fog-client/src/app/models/info-panel-data.ts
+++ dissect-cf-fog-client/src/app/models/info-panel-data.ts
@@ -7,10 +7,11 @@ export function getResourceFilesInfoData(): InfoPanelData {
   return {
     title: 'Resource files',
     text:
-      'A resource file defines how many CPUs and RAMs the actual cloud/fog node deals with.' +
-      'A node considered to be in the lowest fog layer typically utilises the least resources, thus the LPDS_T2 consists of 12 CPU cores and 24 GB RAMs' +
-      'LPDS_T1 file is set to contain resources with 24 CPU cores and 48 GB RAMs and finally LPDS_original deals with 48 CPU cores and 96 GB RAMs' +
-      'The processing power of each CPU core is set to 0.001, which means it processes 0.001 instruction during one tick.'
+      'A resource file defines how many CPUs and RAMs the actual cloud/fog node deals with. <br/>' +
+      'A node considered to be in the lowest fog layer typically utilises the least resources, thus the <b>LPDS_T2</b> consists of 12 CPU cores and 24 GB RAMs <br/>' +
+      '<b>LPDS_T1</b> file is set to contain resources with 24 CPU cores and 48 GB RAMs and finally <b>LPDS_original</b> deals with 48 CPU cores and 96 GB RAMs <br/>' +
+      'The processing power of each CPU core is set to 0.001, which means it processes 0.001 instruction during one tick, which is the'
+      + ' smallest unit of the simulation time. In this environment it is considered to measure time in milliseconds. <br/>'
   } as InfoPanelData;
 }
 
@@ -18,20 +19,22 @@ export function getApplicationInfoData(): InfoPanelData {
   return {
     title: 'Application settings',
     text:
-      'The task size attribute tells the highest amount of unprocessed data that can be packaged in one compute task to be executed by virtual machines.' +
-      'A daemon service chechs regularly the repository for unprocessed data based on the frequency value.' +
-      'The number of instruction defines the maximum value which one task can represent.' +
-      'The threshold determines how many unprocessed task can be hold in the actual application'
-        +'the further tasks are forwarded according to the strategy of the application.' +
-      'The can join allows IoT devices to send unprocessed data directly into the application.' +
-      'The VM flavor is used for executing the compute tasks, which can be specified in the instance tag.' +
-      'You have the following possibilites in case of the strategy value:'+
-      'The Random strategy always chooses one from the connected nodes randomly.' +
-      'The Push Up strategy always chooses the connected parent node (i.e. a node from a higher layer), if available.' +
-      'The Hold Down aims to keep application data as close to the end-user as possible.'+
-      'Finally, the Runtime-aware strategy ranks the available parent nodes, and all neighbour nodes (from its own layer) by network latency' 
-        +'and by the ratio of the available CPU capacity and the total CPU capacity. The algorithm picks the node with the highest rank.' +
-      'Pliant strategy is based on Fuzzy logic, thus load, cost and unprocessed data of a node are considered.'
+      'The <b>task size</b> attribute tells the highest amount of unprocessed data that can be packaged in one compute task to be executed by virtual machines.'
+      +'Suggested value: >5 000 bytes <br/>' +
+      'A daemon service checks regularly the repository for unprocessed data based on the <b>frequency value.</b>.'
+      + 'Suggested value: >6 000 ms <br/>' +
+      'The <b>number of instruction</b> defines the maximum value which one task can represent. Suggested value: >1 000 <br/>' +
+      'The <b>threshold</b> determines how many unprocessed task can be hold in the actual application'
+        +'the further tasks are forwarded according to the strategy of the application. Suggested value: 1-5 <br/>' +
+      'The <b>can join</b> allows IoT devices to send unprocessed data directly into the application. <br/>' +
+      'The VM flavor is used for executing the compute tasks, which can be specified in the <b>instance</b> tag. <br/>' +
+      'You have the following possibilites in case of the strategy value: <br/>'+
+      'The <b>Random</b> strategy always chooses one from the connected nodes randomly. <br/>' +
+      'The <b>Push Up</b> strategy always chooses the connected parent node (i.e. a node from a higher layer), if available. <br/>' +
+      'The <b>Hold Down</b> aims to keep application data as close to the end-user as possible. <br/>'+
+      'The <b>Runtime</b> strategy ranks the available parent nodes, and all neighbour nodes (from its own layer) by network latency' 
+        +'and by the ratio of the available CPU capacity and the total CPU capacity. The algorithm picks the node with the highest rank. <br/>' +
+      '<b>Pliant</b> strategy is based on Fuzzy logic, thus load, cost and unprocessed data of a node are considered. <br/>'
   } as InfoPanelData;
 }
 
@@ -39,17 +42,17 @@ export function getStationInfoData(): InfoPanelData {
   return {
     title: 'Stations',
     text:
-    'We can configure the life time of the device (starttime, stoptime), the number of sensors it has (sensor),' 
-      + 'the size of the generated data (filesize) and the generation and sending frequency.' +
-    'The network  settings  of  the  local  repository  are defined by the maxinbw, maxoutbw and diskbw tags,'
-        + 'and the size of the repository is determined by the reposize field.' +
-    'The radius defines a range where a set of IoT devices are randomly placed,' 
-      +'the number of the devices located at the range is defined by the number attribute.'+
-    'You have the following possibilites in case of the strategy value:'+
-    'The random strategy chooses one from the available applications randomly.' +
-    'The cost-aware strategy looks for the cheapest available application running in any fog/cloud node.'+
-    'The runtime-aware strategy takes into account the actual load of the available nodes.' +
-    'The Fuzzy-based startegy takes in consideration many parameters, such as cost, workload, number of VMs and connected devices, etc.'
+    'We can configure the life time of the device (<b>starttime, stoptime</b> [>1,stoptime>starttime (ms)]), the number of sensors it has (<b>sensor</b> [>0]),' 
+      + 'the size of the generated data (<b>filesize</b> [>49 bytes]) and the generation and sending <b>frequency</b> [>6000 ms]. <br/>' +
+    'The network  settings  of  the  local  repository  are defined by the <b>maxinbw, maxoutbw</b> and <b>diskbw</b> [>1 byte/tick] tags,'
+        + 'and the size of the repository is determined by the <b>reposize</b> [>5000 bytes] field. <br/>' +
+    'The <b>radius</b> defines a range where a set of IoT devices are randomly placed,' 
+      +'the number of the devices located at the range is defined by the <b>number</b> attribute. <br/>'+
+    'You have the following possibilites in case of the strategy value: <br/>'+
+    'The <b>Random</b> strategy chooses one from the available applications randomly. <br/>' +
+    'The <b>Cost</b> strategy looks for the cheapest available application running in any fog/cloud node. <br/>'+
+    'The <b>Runtime</b> strategy takes into account the actual load of the available nodes. <br/>' +
+    'The <b>Fuzzy</b> startegy takes in consideration many parameters, such as cost, workload, number of VMs and connected devices, etc. <br/>'
   } as InfoPanelData;
 }
 
@@ -57,7 +60,7 @@ export function getConectionInfoData(): InfoPanelData {
   return {
     title: 'Connections',
     text:
-      'Parent connection can be made betwen a fog and a cloud node.'+
+      'Parent connection can be made between a fog and a cloud node.'+
       'Anyways you can use the simple connection to create a route among the fog nodes.'
   } as InfoPanelData;
 }
