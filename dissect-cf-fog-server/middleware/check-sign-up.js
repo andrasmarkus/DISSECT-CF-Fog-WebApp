dc53512064e4afdb88c9acd0507f5d4ab56b33e0
Szabolcs <csalik69@gmail.com>
2021-10-05 10:55:18 +0200

    fixing firestore import in check-sign-up.js middleware

--- dissect-cf-fog-server/middleware/check-sign-up.js
+++ dissect-cf-fog-server/middleware/check-sign-up.js
@@ -1,4 +1,6 @@
-const db = require("../models/firestore");
+const { db } = require("../models/firestore");
+
+const User = db.collection('users');
 
 /**
  * Checks the user email exists in database. If it is, it will forward the request.
@@ -10,7 +12,7 @@ const db = require("../models/firestore");
  * @param {Function} next - forwards it
  */
 const checkSignUp = (req, res, next) => {
-  db.user.where('email', '==', req.body.email)
+  User.where('email', '==', req.body.email)
     .get()
     .then((user) => {
       if (!user.empty) {
