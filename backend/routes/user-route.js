9a0b153c360c00c287c92502e2ad60098a2d58f6
Markus Andras <markusa@inf.u-szeged.hu>
2023-07-19 11:49:44 +0200

    fixing build error

--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -10,7 +10,15 @@ const {response} = require("express");
  * Sends all the users from the database with the given response.
  * If comes some error, 500 will be thrown with a message property.
  */
-router.get("/", [authJwt.verifyToken], controller.getAllUser);
+router.get("/", [authJwt.verifyToken], async (req, res) => {
+
+  try {
+    const users = await mongodb.getAllUsers();
+    res.status(200).send({users: users})
+  } catch (e) {
+    res.status(500).send({message: e.message});
+  }
+});
 
 /**
  * Sends back the list of the configurations of the given user.
@@ -95,3 +103,5 @@ async function sendConfig(req,res){
 
   return res.status(201).json({config: config, err: null});
 }
+
+module.exports = router;
